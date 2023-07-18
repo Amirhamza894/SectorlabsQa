086b2945f7243ed6a18810398b964b80650c469f
Shahab <shahabnadeem2010@gmail.com>
2023-07-18 18:23:30 +0500

    Shahab Added a New Test Case for SeleniumActions

--- src/test/java/practice/Shahab.java
+++ src/test/java/practice/Shahab.java
@@ -1,4 +1,43 @@
 package practice;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.Categories;
+
+import java.time.Duration;
+
 public class Shahab {
+
+    private WebDriver driver;
+
+//    private Actions act;
+    @Test
+    public void seleniumAction() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        Actions act = new Actions(driver);
+        act.moveToElement(driver.findElement(By.xpath("//button[@id='mousehover']"))).build().perform();
+        act.moveToElement(driver.findElement(By.xpath("//a[.='Top']"))).click().build().perform();
+        String url = driver.getCurrentUrl();
+        assert url.contains("#top");
+
+
+
+
+
+
+
+
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        category =new Categories(driver);
+//        act = new Actions(driver);
+    }
 }
