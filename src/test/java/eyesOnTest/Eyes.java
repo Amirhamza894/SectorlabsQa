853fbef4adcbc381b3dcb04e4d78b06da3b52df5
amir <haq8948@gmail.com>
2023-07-25 21:45:13 +0500

    test presentation improved. the console will now print and will point the the fail testcase as well as failed step.

--- src/test/java/eyesOnTest/Eyes.java
+++ src/test/java/eyesOnTest/Eyes.java
@@ -5,32 +5,44 @@ import org.apache.logging.log4j.Logger;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import java.awt.desktop.SystemEventListener;
-
 public class Eyes implements ITestListener {
     private static final Logger logger = LogManager.getLogger();
+    private String blueCircle = "\uD83D\uDD35";
+    private String greenTick = "\u2705";
+    private String greenMark = "\u001B[32m";
+    private String boldPass = "\u001B[1m" + " PASSED: " + "\u001B[0m";
+    private String defaultText = "\u001B[0m ";
+    private String redCross = "\u274C";
+    private String redMark = "\u001B[31m";
+    private String boldFail = "\u001B[1m" + " FAILED: " + "\u001B[0m";
+
 
     @Override
     public void onTestStart(ITestResult result) {
-        logger.info("\u2192 TEST: " + result.getMethod().getDescription());
+        logger.info(blueCircle + " TEST: " + result.getMethod().getDescription());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        logger.info("\u2705 PASSED: " + result.getName());
+        logger.info(greenTick +
+                greenMark +
+                boldPass +
+                defaultText +
+                result.getMethod().getDescription());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-
-        System.out.println("\u274C Test FAILED: " + result.getName());
+        logger.info(redCross +
+                redMark +
+                boldFail +
+                defaultText +
+                result.getMethod().getDescription());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-
         System.out.println("Test skipped: " + result.getName());
     }
 
-    // Other methods can also be implemented based on your needs
 }
